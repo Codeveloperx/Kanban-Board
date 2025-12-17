cf9bdedf3eb10ad682dd969cdc849b483415c15a
Camilo Orozco Hdz <camilo.orozcohdz@gmail.com>
2025-12-17 20:52:50 +0100

    Header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,26 @@
+import { Plus } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className="h-14 bg-white border-b border-gray-200 px-6 flex items-center justify-between">
+      <div className="flex items-center space-x-8">
+        <span className="text-xl font-bold text-gray-900">Kanban Board</span>
+      </div>
+      <div className="flex items-center gap-4">
+        <button className="flex items-center space-x-1 cursor-pointer bg-indigo-600 text-white px-3 py-1 rounded shadow hover:bg-indigo-700">
+          <Plus className="w-4 h-4" />
+          <span className="text-sm">New Board</span>
+        </button>
+        <div className="w-8 h-8 bg-gray-300 rounded-full">
+          <img
+            src="https://i.pravatar.cc/40"
+            alt="User Avatar"
+            className="w-8 h-8 rounded-full border"
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

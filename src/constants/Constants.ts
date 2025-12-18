8f548f9d2ea7be1826e153d873db41f5ea0ef16d
Camilo Orozco Hdz <camilo.orozcohdz@gmail.com>
2025-12-18 21:28:43 +0100

    Se crea componente sidebar e items de nevegaciòn

--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -1,4 +1,11 @@
+//BOARD ACTION KEYS
 export const KEY_CREATE_BOARD = "CREATE_BOARD";
 export const KEY_UPDATE_BOARD = "UPDATE_BOARD";
 export const KEY_DELETE_BOARD = "DELETE_BOARD";
 export const KEY_SELECT_BOARD = "SELECT_BOARD";
+
+//NAVIGATION KEYS
+export const KEY_NAV_HOME = "Home";
+export const KEY_NAV_CHATS = "Chats";
+export const KEY_NAV_PROJECTS = "Proyectos";
+export const KEY_NAV_ARTIFACTS = "Artefactos";

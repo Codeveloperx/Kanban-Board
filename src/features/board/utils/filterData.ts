daaef29e78cdb4bcf3ac7528ac8724965cad11ea
Camilo Orozco Hdz <camilo.orozcohdz@gmail.com>
2026-04-11 12:21:37 +0200

    Ajustes en el còdigo

--- src/features/board/utils/filterData.ts
+++ src/features/board/utils/filterData.ts
@@ -7,5 +7,5 @@ const boards = storage.boards.load()?.boards || [];
 export const boardFilter = {
   getActive: (): Board[] => boards.filter((board) => board.active) || [],
   getById: (id: string): Board | null =>
-    boards.find((board) => board.id === id) || null,
+    boards.find((board) => board.id === id && board.active) || null,
 };
